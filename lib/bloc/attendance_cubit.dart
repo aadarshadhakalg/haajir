5738bc8446164c75294a20cfcdb3ccaf028a5fe3
Aadarsha Dhakal <braveboy2058@gmail.com>
2026-08-19 21:06:10 +0545

    geolocator

--- lib/bloc/attendance_cubit.dart
+++ lib/bloc/attendance_cubit.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:haajir/models/attendance_record.dart';
 import 'package:haajir/repositories/attendance_repository.dart';
 
@@ -56,8 +57,30 @@ class AttendanceCubit extends Cubit<AttendanceState> {
 
     try {
       // These are temporary values for the Firestore lesson.
-      await _repository.logToday(livenessPassed: true, photoMatchedScore: 97);
-      emit(AttendanceLoaded(_records));
+      Position currentPostion = await _determinePosition();
+      double collegeLat = 27.7045121;
+      double collegeLong = 85.3324544;
+
+      double distanceFromCollege = Geolocator.distanceBetween(
+        collegeLat,
+        collegeLong,
+        currentPostion.latitude,
+        currentPostion.longitude,
+      );
+
+      print(distanceFromCollege);
+
+      if (distanceFromCollege > 100) {
+        emit(
+          AttendanceError(
+            "You're not in the college premises",
+            previousRecords: _records,
+          ),
+        );
+      } else {
+        await _repository.logToday(livenessPassed: true, photoMatchedScore: 97);
+        emit(AttendanceLoaded(_records));
+      }
     } on FirebaseException catch (error) {
       if (error.code == 'permission-denied') {
         emit(
@@ -80,5 +103,43 @@ class AttendanceCubit extends Cubit<AttendanceState> {
   Future<void> close() async {
     await _subscription?.cancel();
     return super.close();
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.',
+      );
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
